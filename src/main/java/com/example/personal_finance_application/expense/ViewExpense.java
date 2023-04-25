44c3d945d05ef11b5387d7553de3d8c972dedbcd
Girish Madnani <girishmadnani05@gmail.com>
2023-04-25 19:53:55 -0400

    view expense button

--- src/main/java/com/example/personal_finance_application/expense/ViewExpense.java
+++ src/main/java/com/example/personal_finance_application/expense/ViewExpense.java
@@ -1,4 +1,15 @@
 package com.example.personal_finance_application.expense;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
 public class ViewExpense {
+  @FXML
+  private Button goBackButton;
+  
+  @FXML
+  private void onGoBack() {
+    Stage currentStage = (Stage) goBackButton.getScene().getWindow();
+    currentStage.close();
+  }
 }
