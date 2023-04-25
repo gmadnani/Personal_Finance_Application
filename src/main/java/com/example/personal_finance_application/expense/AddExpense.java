9b9830448083cf49f05f69b6fd05ff17f8f0f2f6
Girish Madnani <girishmadnani05@gmail.com>
2023-04-25 19:52:06 -0400

    add expense buttons

--- src/main/java/com/example/personal_finance_application/expense/AddExpense.java
+++ src/main/java/com/example/personal_finance_application/expense/AddExpense.java
@@ -1,4 +1,25 @@
 package com.example.personal_finance_application.expense;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
 public class AddExpense {
+  
+  @FXML
+  private Button saveButton;
+  
+  @FXML
+  private Button cancelButton;
+  
+  @FXML
+  private void onSave() {
+    Stage currentStage = (Stage) saveButton.getScene().getWindow();
+    currentStage.close();
+  }
+  
+  @FXML
+  private void onCancel() {
+    Stage currentStage = (Stage) cancelButton.getScene().getWindow();
+    currentStage.close();
+  }
 }
