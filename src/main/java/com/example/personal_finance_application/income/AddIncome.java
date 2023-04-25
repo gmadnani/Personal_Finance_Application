f0564c63c14890bda2558cf456e62fb57a758580
Girish Madnani <girishmadnani05@gmail.com>
2023-04-25 19:55:39 -0400

    add income buttons

--- src/main/java/com/example/personal_finance_application/income/AddIncome.java
+++ src/main/java/com/example/personal_finance_application/income/AddIncome.java
@@ -1,4 +1,24 @@
 package com.example.personal_finance_application.income;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
 public class AddIncome {
+  @FXML
+  private Button saveButton;
+  
+  @FXML
+  private Button cancelButton;
+  
+  @FXML
+  private void onSave() {
+    Stage currentStage = (Stage) saveButton.getScene().getWindow();
+    currentStage.close();
+  }
+  
+  @FXML
+  private void onCancel() {
+    Stage currentStage = (Stage) cancelButton.getScene().getWindow();
+    currentStage.close();
+  }
 }
