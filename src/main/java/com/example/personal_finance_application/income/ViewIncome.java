2e27481836faca7531943babc90995c5a748d083
Girish Madnani <girishmadnani05@gmail.com>
2023-04-25 19:56:19 -0400

    view income button

--- src/main/java/com/example/personal_finance_application/income/ViewIncome.java
+++ src/main/java/com/example/personal_finance_application/income/ViewIncome.java
@@ -1,4 +1,16 @@
 package com.example.personal_finance_application.income;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
 public class ViewIncome {
+  
+  @FXML
+  private Button goBackButton;
+  
+  @FXML
+  private void onGoBack() {
+    Stage currentStage = (Stage) goBackButton.getScene().getWindow();
+    currentStage.close();
+  }
 }
