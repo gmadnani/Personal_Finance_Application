bff8eeddd7a9e4343aebc21211f674547a42762c
Girish Madnani <girishmadnani05@gmail.com>
2023-04-27 00:40:31 -0400

    add expense function

--- src/main/java/com/example/personal_finance_application/AddExpense.java
+++ src/main/java/com/example/personal_finance_application/AddExpense.java
@@ -4,20 +4,48 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.time.LocalDate;
 import java.util.Objects;
 public class AddExpense {
-  
+  @FXML
+  private TextField categoryField;
+  @FXML
+  private TextField amountField;
+  @FXML
+  private DatePicker dateField;
+  @FXML
+  private TextArea notesField;
   @FXML
   private Button saveButton;
-  
   @FXML
   private Button cancelButton;
   
   @FXML
   private void onSave() throws IOException {
+    // Get the expense data from the user input fields
+    double amount = Double.parseDouble(amountField.getText());
+    String category = categoryField.getText();
+    LocalDate date = dateField.getValue();
+    String notes = notesField.getText();
+    
+    // Add the expense to the user's CSV file
+    File file = new File(Login.getCurrentUser().getEmail() + ".csv");
+    FileWriter writer = new FileWriter(file, true);
+    writer.write("\nExpense: " + amount + "," + category + "," + date + "," + notes);
+    writer.close();
+    
+    // Show a success message and close the window
+    Alert alert = new Alert(Alert.AlertType.INFORMATION, "Expense saved.");
+    alert.showAndWait();
     Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("dashboard.fxml")));
     Stage stage = new Stage();
     stage.setScene(new Scene(root, 600, 800));
