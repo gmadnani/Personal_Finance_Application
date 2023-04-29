43deb3d911653073025f38b4eddc4aaaf7fdd426
Girish Madnani <girishmadnani05@gmail.com>
2023-04-28 22:18:51 -0400

    view expenses function

--- src/main/java/com/example/personal_finance_application/ViewExpense.java
+++ src/main/java/com/example/personal_finance_application/ViewExpense.java
@@ -1,16 +1,79 @@
 package com.example.personal_finance_application;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
-public class ViewExpense {
+public class ViewExpense<T extends ReportEntry> {
+  @FXML
+  private TableView<T> expenseTable;
   @FXML
   private Button goBackButton;
+  
+  public void initialize() throws IOException {
+    updateExpense();
+    showExpense();
+  }
+  
+  private List<T> expenseData = new ArrayList<>();
+  
+  public void updateExpense() throws IOException {
+    
+    File file = new File(Login.getCurrentUser().getEmail() + ".csv");
+    BufferedReader reader = new BufferedReader(new FileReader(file));
+    String line;
+    while ((line = reader.readLine()) != null) {
+      String[] values = line.split(",");
+      String type = values[0];
+      if (type.equals("Expense")) {
+        double amount = Double.parseDouble(values[1].trim());
+        String category = values[2];
+        LocalDate date = LocalDate.parse(values[3]);
+        String notes = values[4];
+        T entry = (T) new ReportEntry(type, amount, category, date, notes);
+        expenseData.add(entry);
+      }
+    }
+    reader.close();
+  }
+  
+  private void showExpense() {
+    TableColumn<T, String> typeCol = new TableColumn<>("Type");
+    typeCol.setCellValueFactory(new PropertyValueFactory<>("type"));
+    
+    TableColumn<T, Double> amountCol = new TableColumn<>("Amount");
+    amountCol.setCellValueFactory(new PropertyValueFactory<>("amount"));
+    
+    TableColumn<T, String> categoryCol = new TableColumn<>("Category");
+    categoryCol.setCellValueFactory(new PropertyValueFactory<>("category"));
+    
+    TableColumn<T, LocalDate> dateCol = new TableColumn<>("Date");
+    dateCol.setCellValueFactory(new PropertyValueFactory<>("date"));
+    
+    TableColumn<T, String> notesCol = new TableColumn<>("Notes");
+    notesCol.setCellValueFactory(new PropertyValueFactory<>("notes"));
+    
+    expenseTable.getColumns().setAll(typeCol, amountCol, categoryCol, dateCol, notesCol);
+    
+    ObservableList<T> data = FXCollections.observableArrayList();
+    data.addAll(expenseData);
+    expenseTable.setItems(data);
+  }
   
   @FXML
   private void onGoBack() throws IOException {
